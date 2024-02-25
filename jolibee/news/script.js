ec157b3b663fde34899aaf93b8be9d259d1cd350
NPhuc17 <126778762+NPhuc17@users.noreply.github.com>
2024-02-25 10:31:04 +0700

    Add files via upload

---
+++ jolibee/news/script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var elements = document.getElementsByClassName("none");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.display = "block";
+    }
+    document.getElementById("load-more").style.display = "none";
+}
+
+function mFunction() {
+  var elements = document.getElementsByClassName("none");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "block";
+  }
+}
